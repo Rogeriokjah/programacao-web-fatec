3b60b9af311ed3e789f5fa90a90811a87dd7e1f6
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-08-08 22:16:03 -0300

    introducao as rotas

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello world"
+@app.route('/') # Devido ao decorador, a função abaixo será executada automaticamente ao chegar nesta rota.
+def route1():
+    return "Rota principal"
 
-app.run()
+@app.route('/rota')
+def route2():
+    return "Rota secundária"
+
+app.run(debug=True)
