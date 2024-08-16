ca6343802c98e0bd92bdeb4196dc65088a13da7e
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-08-15 21:29:01 -0300

    renderizando html

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/') # Devido ao decorador, a função abaixo será executada automaticamente ao chegar nesta rota.
-def route1():
-    return "Rota principal"
+def home():
+    return render_template("index.html") # render_template pega arquivos da pasta "templates"
 
-@app.route('/rota')
-def route2():
-    return "Rota secundária"
-
-app.run(debug=True)
+app.run(debug=True) # ao definir debug como True, a aplicação será recarregada sempre que uma alteração ocorrer
