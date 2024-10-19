4414fd3c1b8916371312fcd7162e90d27f339bcc
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-10-18 21:59:05 -0300

    App com Banco de Dados | Comittando inserts

--- app-banco-de-dados/app.py
+++ app-banco-de-dados/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
 import mysql.connector # pip install mysql-connector-python
 import os
@@ -8,33 +8,37 @@ app = Flask(__name__)
 
 # Testando conexão com o banco de dados
 try:
-    conn = mysql.connector.connect(
-        user=os.getenv('MYSQL_USER'),
-        password=os.getenv('MYSQL_PASSWORD'),
-        host=os.getenv('MYSQL_HOST'),
-        port=os.getenv('MYSQL_PORT'),
-        database=os.getenv('MYSQL_DATABASE')
-    )
+    conn = mysql.connector.connect(user=os.getenv('MYSQL_USER'),password=os.getenv('MYSQL_PASSWORD'),host=os.getenv('MYSQL_HOST'),port=os.getenv('MYSQL_PORT'),database=os.getenv('MYSQL_DATABASE'))
 except:
-    print("Ocorreu um erro ao se conectar com o banco de dados.")
-if conn and conn.is_connected():
-    with conn.cursor() as cursor:
-        result = cursor.execute("SELECT * FROM joaovictor_tbusuario;")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-    conn.close()
+    raise Exception("Ocorreu um erro ao se conectar com o banco de dados.")
+
+# SELECT ALL de teste
+meu_cursor = conn.cursor()
+
+meu_cursor.execute("SELECT * FROM joaovictor_tbusuario;")
+rows = meu_cursor.fetchall()
+for row in rows:
+    print(row)
+
 
 # Rota Principal
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        password = request.form['password']
-        email = request.form['email']
-
-        return render_template("cadastrar-usuario.html")
     return render_template("cadastrar-usuario.html")
+
+
+@app.route("/cadastrar-usuario", methods=['POST'])
+def cadastrar():
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Inserindo no BD:
+        query = "INSERT INTO joaovictor_tbusuario (nome, email, senha) VALUES (%s, %s, %s)"
+        valores = (name, email, password)
+        meu_cursor.execute(query, valores)
+        conn.commit() # Commitando o insert no banco de dados
+        return redirect(url_for('index'))
 
 
 app.run(debug=True)
