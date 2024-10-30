0670bddc8c1a1c67edaac5b31de0407ca239bad3
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-10-29 21:51:28 -0300

    Aula 29.10 | Listagem de usuários

--- app-banco-de-dados/app.py
+++ app-banco-de-dados/app.py
@@ -28,21 +28,43 @@ def index():
 
 
 @app.route("/cadastrar-cliente", methods=['POST'])
-def cadastrar():
-        name = request.form['name']
-        cpf = request.form['cpf']
-        rg = request.form['rg']
-        address = request.form['address']
-        neighborhood = request.form['neighborhood']
-        city = request.form['city']
-        cep = request.form['cep']
+def cadastrar_cliente():
+    name = request.form['name']
+    cpf = request.form['cpf']
+    rg = request.form['rg']
+    address = request.form['address']
+    neighborhood = request.form['neighborhood']
+    city = request.form['city']
+    cep = request.form['cep']
 
-        # Inserindo no BD:
-        query = "INSERT INTO joaovictor_tbcliente (nome, cpf, rg, endereco, bairro, cidade, cep) VALUES (%s, %s, %s,%s, %s, %s,%s)"
-        valores = (name, cpf, rg, address, neighborhood, city, cep)
-        meu_cursor.execute(query, valores)
-        conn.commit() # Commitando o insert no banco de dados
-        return redirect(url_for('index'))
+    # Inserindo no BD:
+    query = "INSERT INTO joaovictor_tbcliente (nome, cpf, rg, endereco, bairro, cidade, cep) VALUES (%s, %s, %s,%s, %s, %s,%s)"
+    valores = (name, cpf, rg, address, neighborhood, city, cep)
+    meu_cursor.execute(query, valores)
+    conn.commit() # Commitando o insert no banco de dados
+    return redirect(url_for('index'))
+
+@app.route("/cadastrar-usuario", methods=['GET'])
+def cadastrar_usuario():
+    return render_template("cadastrar-usuario.html")
+
+@app.route("/cad-user", methods=['POST'])
+def insert_on_database(): # FIXME
+    username = request.form['username']
+    password = request.form['password']
+    query = f"INSERT INTO joaovictor_tbusuario(username, senha) VALUES ('{username}','{password}')"
+    meu_cursor.execute(query)
+    meu_cursor.commit()
+
+    return redirect(url_for('cadastrar_usuario', inseriu=True))
+
+
+@app.route("/list-users", methods=['GET'])
+def list_users():
+    query = f"SELECT * FROM joaovictor_tbusuario"
+    meu_cursor.execute(query)
+    retorno = meu_cursor.fetchall()
+    return render_template("listar-usuarios.html", users=retorno if retorno else None)
 
 @app.route("/login", methods=['GET'])
 def login():
